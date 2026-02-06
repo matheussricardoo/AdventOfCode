93a89d24bfa3f8c710a328559b417c93ac284e29
matheussricardoo <matheusri1@hotmail.com>
2026-02-06 03:41:40 -0300

    Part 1 of day 4, 2015, in Rust

--- 2015/Rust/4/day_four/src/main.rs
+++ 2015/Rust/4/day_four/src/main.rs
@@ -1,3 +1,19 @@
+use md5;
+
 fn main() {
-    println!("Hello, world!");
+    let input = "iwrupvqb";
+    let mut count = 0;
+    loop {
+        let sum_input = format!("{}{}", input, count);
+        let sum_bytes = sum_input.as_bytes();
+
+        let digest = md5::compute(sum_bytes);
+        let convert_hash = format!("{:x}", digest);
+        if convert_hash.starts_with("00000") {
+            print!("{}\n", count);
+            break;
+        } else {
+            count += 1
+        }
+    }
 }
