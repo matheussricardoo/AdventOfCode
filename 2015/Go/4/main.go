9448e8d615e53d5c0cfaa28f18b82d0a7f6af959
matheussricardoo <matheusri1@hotmail.com>
2026-02-06 03:21:56 -0300

    Part 1 of day 4, 2015, in Go

--- 2015/Go/4/main.go
+++ 2015/Go/4/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main () {
-	
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := "iwrupvqb"
+	count := 0
+	for {
+		sum_input := input + strconv.Itoa(count)
+		sum_bytes := []byte(sum_input)
+		hashString := md5.Sum(sum_bytes)
+		convertHash := fmt.Sprintf("%x", hashString)
+		if strings.HasPrefix(convertHash, "00000") {
+			fmt.Printf("The number is: %d\n", count)
+			break
+		} else {
+			count++
+		}
+	}
 }
