71e41d77f9eaa7597249d07840568217d0967e26
matheussricardoo <matheusri1@hotmail.com>
2026-02-09 22:39:47 -0300

    Implementing part 1 of day 5, 2015, in Rust and adding the input file

--- 2015/Rust/5/day_five/src/main.rs
+++ 2015/Rust/5/day_five/src/main.rs
@@ -1,3 +1,35 @@
+use std::{fs, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = Path::new("src/input.txt");
+    let raw_text = fs::read_to_string(file_path).unwrap();
+
+    let mut good_strings = 0;
+
+    for line in raw_text.lines() {
+        let mut vowels = 0;
+        let mut has_double = false;
+        let mut has_forbidden = false;
+        let mut last_char = '\0';
+        for ch in line.chars() {
+            if ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' {
+                vowels += 1
+            }
+            if ch == last_char {
+                has_double = true
+            }
+            if last_char == 'a' && ch == 'b'
+                || last_char == 'c' && ch == 'd'
+                || last_char == 'p' && ch == 'q'
+                || last_char == 'x' && ch == 'y'
+            {
+                has_forbidden = true
+            }
+            last_char = ch;
+        }
+        if vowels >= 3 && has_double == true && has_forbidden == false {
+            good_strings += 1
+        }
+    }
+    println!("{}", good_strings);
 }
