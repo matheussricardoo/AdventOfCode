483e3a6407fbd9f2e27f31ad07d7e15c1c51ef48
matheussricardoo <matheusri1@hotmail.com>
2026-02-10 22:56:58 -0300

    Implementing part 1 of day 6,2015,in Rust

--- 2015/Rust/6/part1/src/main.rs
+++ 2015/Rust/6/part1/src/main.rs
@@ -1,3 +1,49 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_text = include_str!("input.txt");
+    let mut total_lights = 0;
+    let mut grid: [[usize; 1000]; 1000] = [[0; 1000]; 1000];
+    let re = Regex::new(r"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)").unwrap();
+    for line in raw_text.lines() {
+        if let Some(caps) = re.captures(line) {
+            let command = &caps[1];
+            let x1: usize = caps[2].parse().unwrap();
+            let y1: usize = caps[3].parse().unwrap();
+            let x2: usize = caps[4].parse().unwrap();
+            let y2: usize = caps[5].parse().unwrap();
+
+            let state = if command == "turn on" {
+                "ON"
+            } else if command == "turn off" {
+                "OFF"
+            } else {
+                "REVERT"
+            };
+
+            for i in x1..=x2 {
+                for j in y1..=y2 {
+                    if state == "ON" {
+                        grid[i][j] = 1;
+                    } else if state == "OFF" {
+                        grid[i][j] = 0
+                    } else {
+                        if grid[i][j] == 1 {
+                            grid[i][j] = 0
+                        } else {
+                            grid[i][j] = 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for i in 0..(1000) {
+        for j in 0..(1000) {
+            if grid[i][j] == 1 {
+                total_lights += 1
+            }
+        }
+    }
+    println!("{}", total_lights)
 }
