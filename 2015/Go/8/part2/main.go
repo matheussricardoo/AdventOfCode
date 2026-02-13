fd85183787b95dc8f82fcec031e3c4b533e494dd
matheussricardoo <matheusri1@hotmail.com>
2026-02-13 00:53:57 -0300

    Implementing part 2 of day 8,2015,in Go

--- 2015/Go/8/part2/main.go
+++ 2015/Go/8/part2/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	codeCount := 0
+	encodedCount := 0
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		codeCount += len(input)
+		encodedString := strconv.Quote(input)
+		encodedCount += len(encodedString)
+	}
+	totalDiff := encodedCount - codeCount
+	fmt.Printf("Total difference: %d\n", totalDiff)
 
 }
