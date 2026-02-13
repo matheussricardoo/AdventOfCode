2f9475f9a4f7dc16e7cea682243d4fe4272461b5
matheussricardoo <matheusri1@hotmail.com>
2026-02-13 01:13:03 -0300

    Implementing part 1 of day 8,2015,in Rust

--- 2015/Rust/8/part1/src/main.rs
+++ 2015/Rust/8/part1/src/main.rs
@@ -1,3 +1,35 @@
+fn unescape(input: &str) -> usize {
+    let content = &input[1..input.len() - 1];
+    let mut count = 0;
+    let mut chars = content.chars();
+
+    while let Some(c) = chars.next() {
+        if c == '\\' {
+            match chars.next() {
+                Some('x') => {
+                    chars.next();
+                    chars.next();
+                    count += 1;
+                }
+                Some(_) => {
+                    count += 1;
+                }
+                None => {}
+            }
+        } else {
+            count += 1;
+        }
+    }
+    count
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw_text = include_str!("input.txt");
+    let mut code_count = 0;
+    let mut mem_string_count = 0;
+    for line in raw_text.lines() {
+        code_count += line.len();
+        mem_string_count += unescape(line);
+    }
+    println!("Total difference: {}", code_count - mem_string_count);
 }
