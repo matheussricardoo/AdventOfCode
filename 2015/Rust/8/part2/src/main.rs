24dc3fcd7821b6f63339b73020fadd65a9c0eec7
matheussricardoo <matheusri1@hotmail.com>
2026-02-13 01:25:29 -0300

    Implementing part 2 of day 8,2015,in Rust

--- 2015/Rust/8/part2/src/main.rs
+++ 2015/Rust/8/part2/src/main.rs
@@ -1,3 +1,23 @@
+fn encoded_size(input: &str) -> usize {
+    let mut count = 2;
+
+    for c in input.chars() {
+        match c {
+            '"' | '\\' => count += 2,
+            _ => count += 1,
+        }
+    }
+    count
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw_text = include_str!("input.txt");
+    let mut code_count = 0;
+    let mut total_encoded = 0;
+
+    for line in raw_text.lines() {
+        code_count += line.len();
+        total_encoded += encoded_size(line);
+    }
+    println!("Total difference: {}", total_encoded - code_count);
 }
